c54a2fbe292c0f0c89c1ed01a518333000ff13aa
Zhenyu Fu <zhenyufu@usc.edu>
2017-02-11 20:23:54 -0800

    fixed dropdown lingering

--- js/gui.js
+++ js/gui.js
@@ -1,6 +1,7 @@
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
 function myFunction(name) {
+    closeDropdown();
     str = "menu-dropdown-" + name;
     document.getElementById(str).classList.toggle("show");
 }
@@ -8,14 +9,18 @@ function myFunction(name) {
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(e) {
   if (!e.target.matches('.dropbtn')) {
-
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    for (var d = 0; d < dropdowns.length; d++) {
-      var openDropdown = dropdowns[d];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
+    closeDropdown();
   }
 }
 
+function closeDropdown(){
+var dropdowns = document.getElementsByClassName("dropdown-content");
+     for (var d = 0; d < dropdowns.length; d++) {
+       var openDropdown = dropdowns[d];
+       if (openDropdown.classList.contains('show')) {
+         openDropdown.classList.remove('show');
+       }
+     }
+
+
+}
