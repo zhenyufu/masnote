354fd21579222fe1f8770b430a7c1756ce407a51
Zhenyu Fu <zhenyufu@usc.edu>
2017-04-12 22:52:55 -0700

    commented out nodegit, it give blank

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@ var exec = require('child_process').exec;
 const Config = require('electron-config');
 const config = new Config();
 const Path = require('path');
+//const Git = require("nodegit");
+
 //const tinymce = require("tinymce");
 /* 
  *  Init
@@ -271,8 +273,43 @@ function handleButtonDownloadBook(){
      title: 'Download Book',
      body: {type: 'textbox', name:"masName", label:"Enter url"},
      onsubmit: function(e) {
-         var newName = e.data.masName;
-            
+         var bookUrl = e.data.masName;
+
+
+/*
+
+
+         ///////////////////////////
+        Git.Clone(bookUrl, workspacePath)
+            .then(function(repo) {
+                return repo.getMasterCommit();  //getCommit("59b20b8d5c6ff8d09518454d4dd8b7b30f095ab5");
+            })
+            .then(function(commit) {
+                return commit.getEntry("README.md");
+            })
+            // Get the blob contents from the file.
+            .then(function(entry) {
+                // Patch the blob to contain a reference to the entry.
+                return entry.getBlob().then(function(blob) {
+                    blob.entry = entry;
+                    return blob;
+                });
+            })
+            // Display information about the blob.
+            .then(function(blob) {
+                console.log(blob.entry.path() + blob.entry.sha() + blob.rawsize() + "b");
+                // Show a spacer.
+                console.log(Array(72).join("=") + "\n\n");
+                // Show the entire file.
+                console.log(String(blob));
+            })
+            .catch(function(err) { console.log(err); }); 
+         
+         
+        */ 
+         
+         
+         /////////////////////////
      }// onsubmit
    });
 
